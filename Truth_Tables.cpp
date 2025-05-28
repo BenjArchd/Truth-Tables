514535f6640ecd7ab8326f1f1ce814af12947d7f
Benjamin Archard <archardben@outlook.com>
2025-05-28 23:48:24 +0100

    Update Truth_Tables.cpp

--- Truth_Tables.cpp
+++ Truth_Tables.cpp
@@ -2,6 +2,9 @@
    \      TRUTH TABLES FOR AND, NOT and OR  - Benjamin Archard   \
    \       - Inefficient method but used so I can learn          \
     -------------------------------------------------------------
+    -> INTENTIONAL
+    - USING THE HEAP INSTEAD OF THE STACK
+    - 'Tables' ISN'T RETURNING 
 */
 
 
@@ -49,6 +52,8 @@ unsigned& b4, unsigned& b5, unsigned& b6, unsigned& b7, int mode) {
             */
 
 
+            // Should really be on the stack but using the heap for learning
+
             Tables* bitTable;
             bitTable = new Tables;
             bitTable->wholeByte = 0; // Sets the byte to 0
@@ -66,16 +71,21 @@ unsigned& b4, unsigned& b5, unsigned& b6, unsigned& b7, int mode) {
             bitTable->bits.bit6 = (b6 == 0) ? 1 : 0;
             bitTable->bits.bit7 = (b7 == 0) ? 1 : 0;
 
+            // COULD BE DONE USING A LOOP
+
             std::cout << "INPUTS " << "\t" << " OUTPUTS " << std::endl; 
 
-            std::cout << (int)b0 << "\t" << (int)bitTable->bits.bit0 << std::endl;
-            std::cout << (int)b1 << "\t" << (int)bitTable->bits.bit1 << std::endl;
-            std::cout << (int)b2 << "\t" << (int)bitTable->bits.bit2 << std::endl;
-            std::cout << (int)b3 << "\t" << (int)bitTable->bits.bit3 << std::endl;
-            std::cout << (int)b4 << "\t" << (int)bitTable->bits.bit4 << std::endl;
-            std::cout << (int)b5 << "\t" << (int)bitTable->bits.bit5 << std::endl;
-            std::cout << (int)b6 << "\t" << (int)bitTable->bits.bit6 << std::endl;
-            std::cout << (int)b7 << "\t" << (int)bitTable->bits.bit7 << std::endl;
+            std::cout << b0 << "\t" << bitTable->bits.bit0 << std::endl;
+            std::cout << b1 << "\t" << bitTable->bits.bit1 << std::endl;
+            std::cout << b2 << "\t" << bitTable->bits.bit2 << std::endl;
+            std::cout << b3 << "\t" << bitTable->bits.bit3 << std::endl;
+            std::cout << b4 << "\t" << bitTable->bits.bit4 << std::endl;
+            std::cout << b5 << "\t" << bitTable->bits.bit5 << std::endl;
+            std::cout << b6 << "\t" << bitTable->bits.bit6 << std::endl;
+            std::cout << b7 << "\t" << bitTable->bits.bit7 << std::endl;
+
+            delete bitTable; // Frees the memory
+            bitTable = nullptr; // Stops a dangling pointer
 
             break;
         }
@@ -113,20 +123,23 @@ unsigned& b4, unsigned& b5, unsigned& b6, unsigned& b7, int mode) {
             // Splits the byte into 2 nibbles aswell as checks them as an AND Gate
 
             // The check is done by adding the two bits together ~
-            /* E.G if (int)b0 == 1 and (int)b1 == 1 then by adding them the result should be two,
+            /* E.G if b0 == 1 and b1 == 1 then by adding them the result should be two,
                therefore the AND Gate will pass it as a 1. Otherwise a 0 
             */
 
-            bitTable->bits.nib0 = (((int)b0 + (int)b1) == 2) ? 1 : 0; //nib0 is the first two bits of the byte 
-            bitTable->bits.nib1 = (((int)b2 + (int)b3) == 2) ? 1 : 0; //nib1 is the third and forth bits the byte
-            bitTable->bits.nib2 = (((int)b4 + (int)b5) == 2) ? 1 : 0; //nib2 is the fith and sixth bits of the byte
-            bitTable->bits.nib3 = (((int)b6 + (int)b7) == 2) ? 1 : 0; //nib4 is the seventh and eighth bits of the byte 
+            bitTable->bits.nib0 = ((b0 + b1) == 2) ? 1 : 0; //nib0 is the first two bits of the byte 
+            bitTable->bits.nib1 = ((b2 + b3) == 2) ? 1 : 0; //nib1 is the third and forth bits the byte
+            bitTable->bits.nib2 = ((b4 + b5) == 2) ? 1 : 0; //nib2 is the fith and sixth bits of the byte
+            bitTable->bits.nib3 = ((b6 + b7) == 2) ? 1 : 0; //nib4 is the seventh and eighth bits of the byte 
 
             std::cout << "INPUTS " << "\t" << " OUTPUTS " << std::endl; 
-            std::cout << (int)b0 << " " << (int)b1 << "\t" << (int)bitTable->bits.nib0 << std::endl;
-            std::cout << (int)b2 << " " << (int)b3 << "\t" << (int)bitTable->bits.nib1 << std::endl;
-            std::cout << (int)b4 << " " << (int)b5 << "\t" << (int)bitTable->bits.nib2 << std::endl;
-            std::cout << (int)b6 << " " << (int)b7 << "\t" << (int)bitTable->bits.nib3 << std::endl;
+            std::cout << b0 << " " << b1 << "\t" << bitTable->bits.nib0 << std::endl;
+            std::cout << b2 << " " << b3 << "\t" << bitTable->bits.nib1 << std::endl;
+            std::cout << b4 << " " << b5 << "\t" << bitTable->bits.nib2 << std::endl;
+            std::cout << b6 << " " << b7 << "\t" << bitTable->bits.nib3 << std::endl;
+
+            delete bitTable; // Frees the memory
+            bitTable = nullptr; // Stops a dangling pointer
 
             break;
         }
@@ -160,19 +173,22 @@ unsigned& b4, unsigned& b5, unsigned& b6, unsigned& b7, int mode) {
             // CHECKS IF EITHER OF THE BITS ARE '1' 
             // IF EITHER ONE OF THE BITS ARE '1' THEN INITIALISE nib'0 to 3' TO '1'
             // ELSE SET IT TO 0
-            bitTable->bits.nib0 = (((int)b0 == 1) || ((int)b1 == 1)) ? 1 : 0; 
-            bitTable->bits.nib1 = (((int)b2 == 1) || ((int)b3 == 1)) ? 1 : 0;
-            bitTable->bits.nib2 = (((int)b4 == 1) || ((int)b5 == 1)) ? 1 : 0;
-            bitTable->bits.nib0 = (((int)b6 == 1) || ((int)b7 == 1)) ? 1 : 0;   
+            bitTable->bits.nib0 = ((b0 == 1) || (b1 == 1)) ? 1 : 0; 
+            bitTable->bits.nib1 = ((b2 == 1) || (b3 == 1)) ? 1 : 0;
+            bitTable->bits.nib2 = ((b4 == 1) || (b5 == 1)) ? 1 : 0;
+            bitTable->bits.nib3 = ((b6 == 1) || (b7 == 1)) ? 1 : 0;   
 
             std::cout << "INPUTS " << "\t" << " OUTPUTS " << std::endl; 
 
-            std::cout << (int)b0 << " " << (int)b1 << "\t" << (int)bitTable->bits.nib0 << std::endl;
-            std::cout << (int)b2 << " " << (int)b3 << "\t" << (int)bitTable->bits.nib1 << std::endl;
-            std::cout << (int)b4 << " " << (int)b5 << "\t" << (int)bitTable->bits.nib2 << std::endl;
-            std::cout << (int)b6 << " " << (int)b7 << "\t" << (int)bitTable->bits.nib0 << std::endl;
+            std::cout << b0 << " " << b1 << "\t" << bitTable->bits.nib0 << std::endl;
+            std::cout << b2 << " " << b3 << "\t" << bitTable->bits.nib1 << std::endl;
+            std::cout << b4 << " " << b5 << "\t" << bitTable->bits.nib2 << std::endl;
+            std::cout << b6 << " " << b7 << "\t" << bitTable->bits.nib3 << std::endl;
 
-            //break;
+            delete bitTable; // Frees the memory
+            bitTable = nullptr; // Stops a dangling pointer
+
+            break;
         }
     }
 }
@@ -189,10 +205,10 @@ int main() {
 
     // Enter bits
     for (int i = 0; i < 8; i++) {
-        std::cout << "Enter value for the " << i << " bit: "; std::cin >> binaryString[i];
-        while (binaryString[i] < 0 || binaryString[i] > 1) {
+        do {
             std::cout << "Enter value for the " << i << " bit: "; std::cin >> binaryString[i];
         }
+        while (binaryString[i] < 0 || binaryString[i] > 1);
     }
 
     assignBits(binaryString[0], binaryString[1], binaryString[2], binaryString[3], binaryString[4],
